bcd15ce0de588fbeef4cb58adafcd807e2129f54
profit15 <cassianotrevisan@yahoo.com.br>
2015-04-09 22:43:37 -0300

    sss

--- appExpress.js
+++ appExpress.js
@@ -39,8 +39,13 @@ app.use(multer({ dest: './uploads/',
 
 }));
 
-app.listen(8080);
-
+//app.listen(8080);
+var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
+var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
+ 
+server.listen(server_port, server_ip_address, function () {
+  console.log( "Listening on " + server_ip_address + ", server_port " + port )
+});
 
 var nodemailer = require('nodemailer');
 var transporter = nodemailer.createTransport({
